5a8e8ee00e9f1a63165c214147dbb3165cc914a3
shanmathi <shanmathidevan@gmail.com>
2024-05-28 13:02:24 +0530

    commiting the updated

--- src/test/java/rahulshettyacademy/tests/StandAloneTest.java
+++ src/test/java/rahulshettyacademy/tests/StandAloneTest.java
@@ -18,7 +18,7 @@ public class StandAloneTest {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-
+//checking CICD pipeline 
 		String productName = "ZARA COAT 3";
 		WebDriverManager.chromedriver().setup();
 
